6ec3be53d4e74fa04f74ce161e1c0e68fed4a0ca
CristhianMotoche <cristhian.motoche@gmail.com>
2023-06-26 20:43:54 -0500

    feat: Connect with database

---
+++ likes/README.md
@@ -0,0 +1,17 @@
+# Likes
+
+Simple App to track likes of people.
+
+## Requirements
+
+* asdf
+
+## Start
+
+```
+asdf install
+python -v venv venv
+source venv/bin/activate.fish
+pip install -r requirements.txt
+prisma db push
+```
--- likes/main.py
+++ likes/main.py
@@ -1,11 +1,21 @@
 from typing import Literal
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
+from prisma import Prisma
 
 app = FastAPI()
 
+prisma = Prisma()
+
 
 LikeType = Literal["food", "sports"]
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    await prisma.connect()
+    response = await call_next(request)
+    await prisma.disconnect()
+    return response
 
 class Like(BaseModel):
     id: int
@@ -16,10 +26,22 @@ class Person(BaseModel):
     id: int
     likes: list[Like]
 
+@app.get("/persons")
+async def all_person() -> list[Person]:
+    users = await prisma.user.find_many()
+    return list(map(lambda x: Person(id=x.id, likes=[]), users))
+
 @app.get("/persons/{item_id}")
-def read_persons(item_id: int) -> Person:
-    return Person(id=item_id, likes=[Like(id=1, name="Pineable", type_="food")])
+async def get_person(item_id: int) -> Person:
+    userOp = await prisma.user.find_first(where={ 'id': item_id })
+    if not userOp:
+        raise HTTPException(status_code=404, detail="Person not found")
+    return Person(id=userOp.id, likes=[])
 
 @app.post("/persons")
-def add_persons(person: Person) -> Person:
+async def add_persons(person: Person) -> Person:
+    user = await prisma.user.create(
+        data={ 'name': f'User #{person.id}' },
+    )
+    print('CL - user', user)
     return person
