47cb3ae99a4c2d1650c04f606b64997b6ccead58
CristhianMotoche <cristhian.motoche@gmail.com>
2024-03-20 20:25:40 -0500

    feat: Solve issue with templates

--- ecu-cities-htmx/devenv.nix
+++ ecu-cities-htmx/devenv.nix
@@ -1,8 +1,11 @@
 { pkgs, ... }:
 
+let
+  pwd = builtins.getEnv "PWD";
+in
 {
   # https://devenv.sh/basics/
-  env.GREET = "hello";
+  env.TEMPLATE_DIR = "${pwd}/templates";
 
   # https://devenv.sh/packages/
   packages = [ pkgs.git ];
@@ -11,7 +14,15 @@
   languages.python.enable = true;
   languages.python.version = "3.12.0";
   languages.python.venv = {
-	  enable = true;
-	  requirements = ./requirements.txt;
+    enable = true;
+    requirements = ./requirements.txt;
   };
+
+  # scripts
+  scripts.run.exec = ''
+    python dist
+  '';
+  scripts.compile-watch.exec = ''
+    coconut . dist -w
+  '';
 }
